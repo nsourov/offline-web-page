d034103205986117bb95ebbf10174b4b43ec9541
Naimur Rahman <naimur.rahman103@gmail.com>
2023-09-02 15:22:47 +0600

    🚀

---
+++ sw.js
@@ -0,0 +1,40 @@
+const cacheName = "offline-cache-v1";
+const cacheUrls = ["index.html", "cat.jpeg"];
+
+// Installing the Service Worker
+self.addEventListener("install", async (event) => {
+  try {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(cacheUrls);
+  } catch (error) {
+    console.error("Service Worker installation failed:", error);
+  }
+});
+
+// Fetching resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName);
+
+      try {
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+          console.log("cachedResponse: ", event.request.url);
+          return cachedResponse;
+        }
+
+        const fetchResponse = await fetch(event.request);
+        if (fetchResponse) {
+          console.log("fetchResponse: ", event.request.url);
+          await cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        }
+      } catch (error) {
+        console.log("Fetch failed: ", error);
+        const cachedResponse = await cache.match("index.html");
+        return cachedResponse;
+      }
+    })()
+  );
+});
